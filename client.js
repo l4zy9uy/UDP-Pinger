686ef305a93db707e4944c167d012d523ee36d7a
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-06-14 09:06:14 +0700

    fix: client sends message in the same timestamp

--- client.js
+++ client.js
@@ -2,7 +2,6 @@ const UDP = require('dgram');
 const client = UDP.createSocket('udp4');
 const port = 3000;
 const hostname = process.argv[2];
-const date = new Date();
 
 client.on('message', (mes, rInfo) => {
     const sentTime = mes.toString().split(' ')[2];
@@ -14,7 +13,9 @@ client.on('message', (mes, rInfo) => {
 
 for (let i = 1; i <= 10; i++) {
     setTimeout(() => {
-        sendPing(i);
+        const date = new Date();
+        const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`;
+        sendPing(i, time);
         client.once('message', () => {
             clearTimeout(timeoutId);
         });
@@ -29,8 +30,7 @@ setTimeout(() => {
     client.close();
 }, 12000);
 
-function sendPing(sequenceNumber) {
-    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`;
+function sendPing(sequenceNumber, time) {
     const message = Buffer.from(`Ping ${sequenceNumber} ${time}`);
     client.send(message, 0, message.length, port, hostname, (err) => {
         if (err) {
@@ -42,7 +42,8 @@ function sendPing(sequenceNumber) {
 
 function calculateTime(sentTime) {
     const timePart = sentTime.split(':');
-    const time = parseInt(timePart[3]) + 1000*(parseInt(timePart[2]) + parseInt(timePart[1]*60) + parseInt(timePart[0])*60*60);
+    const time = parseInt(timePart[3]) + 1000*(parseInt(timePart[2]) + parseInt(timePart[1])*60 + parseInt(timePart[0])*60*60);
+    const date = new Date();
     const curTime = date.getMilliseconds() + 1000*(date.getSeconds() + date.getMinutes()*60 + date.getHours()*60*60);
     return curTime - time;
 }
